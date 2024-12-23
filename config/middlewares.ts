1252a32ca00be5006281f61dec70d3d76f78f5b6
AguzzTN54 <gusagusnanang@gmail.com>
2024-12-24 04:03:41 +0700

    Oracle Bucket Provider

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,11 @@
-export default [
+const getOrigin = (url: string = ''): string => {
+  if (!url) return '';
+  return url.replace(/^https?:\/\//, '');
+};
+
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +13,33 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            getOrigin(env('OCI_ENDPOINT')),
+            getOrigin(env('CF_PUBLIC_ACCESS_URL')),
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            getOrigin(env('OCI_ENDPOINT')),
+            getOrigin(env('CF_PUBLIC_ACCESS_URL')),
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
+
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,31 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env('OCI_ACCESS_KEY_ID'),
+            secretAccessKey: env('OCI_ACCESS_SECRET'),
+          },
+          region: env('OCI_REGION'),
+          endpoint: env('OCI_ENDPOINT'),
+          forcePathStyle: true,
+          signatureVersion: 'v4',
+          params: {
+            ACL: env('OCI_VISIBILITY', 'public-read'), // <== set ACL to private
+            signedUrlExpires: env('OCI_SIGNED_URL_EXPIRES', 15 * 60),
+            Bucket: env('OCI_BUCKET'),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
+
